27e1a91f81eb8b12daca32b79721702519e0e1b6
niexiawei <1135215907@qq.com>
2025-11-26 20:19:12 +0800

    update

--- asaserver/server.go
+++ asaserver/server.go
@@ -304,24 +304,8 @@ func IsServerRunning(instanceName string) (bool, error) {
 			}
 		}
 	}
-	logger.GetLogger().Warnf("Game port :%d not found", config.Port)
+	//logger.GetLogger().Warnf("Game port :%d not found", config.Port)
 	return false, nil
-
-	//return 0, fmt.Errorf("no process found listening on port %d", port)
-
-	// Check if both the game port and RCON port are in the output
-	//gamePortStr := fmt.Sprintf("*:%d", config.Port)
-	//gamePortStr2 := fmt.Sprintf("0.0.0.0:%d", config.Port)
-	//// Both ports must be present in the netstat output for the server to be considered running
-	//hasGamePort := strings.Contains(netstatOutput, gamePortStr)
-	//hasGamePort2 := strings.Contains(netstatOutput, gamePortStr2)
-	//
-	//if !hasGamePort && !hasGamePort2 {
-	//	logger.GetLogger().Warnf("Game port :%d not found", config.Port)
-	//	return false, nil
-	//}
-	//
-	//return true, nil
 }
 
 // CopyDir copies a directory recursively
