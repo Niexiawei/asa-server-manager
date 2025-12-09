647f0a3db5a8e7c0ff9c5141beca93e1829be018
niexiawei <1135215907@qq.com>
2025-12-09 10:07:48 +0800

    update:package.json

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,10 +1,19 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import {fileURLToPath, URL} from 'node:url'
+import terser from '@rollup/plugin-terser'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
+    build: {
+        minify: 'terser',
+        terserOptions: {
+            compress: {
+                drop_console: true
+            }
+        }
+    },
     server: {
         host: '0.0.0.0',
         port: 3000,
