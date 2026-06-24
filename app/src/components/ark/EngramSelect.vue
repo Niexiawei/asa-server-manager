4ddbf78d245f54fb0e9285647606e61e9f8d4e4e
niexiawei <1135215907@qq.com>
2026-06-24 20:30:42 +0800

    新增印痕的下拉选择

---
+++ app/src/components/ark/EngramSelect.vue
@@ -0,0 +1,15 @@
+<template>
+  <ark-class-select
+      :model-value="modelValue"
+      dataset="engrams"
+      placeholder="搜索印痕名称或 ClassName"
+      @update:model-value="(v) => emit('update:modelValue', v)"
+  />
+</template>
+
+<script setup>
+import ArkClassSelect from './ArkClassSelect.vue'
+
+defineProps({modelValue: {type: String, default: ''}})
+const emit = defineEmits(['update:modelValue'])
+</script>
